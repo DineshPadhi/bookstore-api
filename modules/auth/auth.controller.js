eef05505f8e42f7a6934f0dbbcf9291edc6d4d25
Tejas Desai <tejas.desai@vernost.in>
2023-01-10 19:38:10 +0530

    Adds auth routes

---
+++ modules/auth/auth.controller.js
@@ -0,0 +1,84 @@
+const bcrypt = require('bcrypt')
+const knex = require('../../config/knex')
+const jwt = require('jsonwebtoken')
+const User = require('../../models/user.model')
+
+async function login(req, res) {
+
+    const email = req.body.email
+    const password = req.body.password
+
+    let user = await User.findOne({email: email})
+
+    console.log(user)
+
+    if(!user) {
+        res.send({
+            error: true,
+            message: "Invalid email or password"
+        })
+        return
+    }
+
+    let matched = await bcrypt.compare(password, user.password);
+
+    if(!matched) {
+        res.send({
+            error: true,
+            message: "Invalid email or password"
+        })
+        return
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+        },
+        "secret"
+    );
+
+
+    res.send({
+        error: false,
+        token: userJwt,
+        message: "Login successfull"
+    })
+
+}
+
+async function signup(req, res) {
+
+    const email = req.body.email
+    const password = req.body.password
+
+    let user = await User.findOne({email: email})
+
+    if(user) {
+        res.send({
+            error: true,
+            message: "Email already exists"
+        })
+        return
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    let newUser = {
+        email,
+        password: hashedPassword
+    }
+
+    await User.create(newUser)
+
+    res.send({
+        error: false,
+        message: "Account created successfully"
+    })
+}
+
+module.exports = {
+    login,
+    signup
+}
